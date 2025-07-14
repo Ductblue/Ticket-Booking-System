766036cddf1e8c1e2024b3c1b367d669c24a8c93
Ductblue <149589913+Ductblue@users.noreply.github.com>
2025-07-14 17:21:45 +0530

    Added all the files
    Added all the files of this IRCTC type train ticket booking system.

---
+++ lib/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,72 @@
+package ticket.booking.entities;
+
+public class Ticket {
+
+    private String ticketID;
+    private String userID;
+    private String source;
+    private String destination;
+    private String dateOfTravel;
+    private Train train;
+
+    public Ticket(String ticketID, String userID, String source, String destination, String dateOfTravel, Train train) {
+        this.ticketID = ticketID;
+        this.userID = userID;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s .", ticketID,userID,source,destination,dateOfTravel);
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+}
